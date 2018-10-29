b45f026097a4635e340e78482397a2a76103188b
Timothy Georgeson <timothy.georgeson@mobileappscompany.us>
2018-10-29 14:55:05 -0400

    Initial commit

---
+++ app/src/main/java/com/example/user/handlingactivitylifecycle/model/Person.java
@@ -0,0 +1,48 @@
+package com.example.user.handlingactivitylifecycle.model;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+    String name;
+    String age;
+    String gender;
+
+    public Person(String name, String age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
